e8c719a9add2a8fe2e6bd9eab6b13f394364f357
krove44 <krove8@gmail.com>
2026-04-26 15:39:13 +0300

    clean repo

--- build/tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "C:/Users/krove44/CLionProjects/second_laba/build/tests[1]_tests.cmake")
-  include("C:/Users/krove44/CLionProjects/second_laba/build/tests[1]_tests.cmake")
-else()
-  add_test(tests_NOT_BUILT tests_NOT_BUILT)
-endif()
